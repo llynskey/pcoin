a37c97104f1df75fcb34a33af15fe1d3f485fe31
lawrence <lawrence1910@googlemail.com>
2021-06-18 18:50:21 +0100

    Vendor Retrieval Service

---
+++ vendorRetrievalService/Controllers/vendorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using vendorRetrievalService.Models;
+using vendorRetrievalService.Services;
+
+namespace vendorRetrievalService.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class vendorController : ControllerBase
+    {
+        private readonly VendorService _vendorService;
+
+        public vendorController(VendorService VendorService)
+        {
+            _vendorService = VendorService;
+        }
+
+        [HttpGet]
+        public ActionResult<Vendor> getVendor(Vendor vendor){
+            Vendor Vendor = _vendorService.Get(vendor._id);
+            Vendor.Pass = null;
+            return Ok(Vendor);
+        }
+
+    }
+}
---
+++ vendorRetrievalService/Services/VendorService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using vendorRetrievalService.Models;
+using MongoDB.Driver;
+
+namespace vendorRetrievalService.Services
+{
+    public class VendorService
+    {
+        private readonly IMongoCollection<Vendor> _vendors;
+
+        public VendorService(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _vendors = database.GetCollection<Vendor>(settings.CollectionName);
+        }
+
+        public List<Vendor> Get() =>
+            _vendors.Find(vendor => true).ToList();
+
+        public Vendor Get(string _id) =>
+            _vendors.Find<Vendor>(vendor => vendor._id == _id).FirstOrDefault();
+    }
+}
