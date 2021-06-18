5b729c66c0ff29f1ebe966315a6610c4e087963b
lawrence <lawrence1910@googlemail.com>
2021-06-18 18:52:08 +0100

    Customer Management Service

---
+++ customerManagementService/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using customerManagementService.Models;
+using customerManagementService.Services;
+
+namespace customerManagementService.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly CustomerService _customerService;
+        
+        public AccountController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public ActionResult<Customer> CreateCustomer(Customer customer)
+        {
+           _customerService.Create(customer);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult<Customer> EditCustomer(Customer customer)
+        {
+            _customerService.Edit(customer);
+            return Ok();
+        }
+    }
+}
---
+++ customerManagementService/Services/CustomerService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using customerManagementService.Models;
+using MongoDB.Bson;
+
+namespace customerManagementService.Services
+{
+      public class CustomerService
+      {
+            private readonly IMongoCollection<Customer> _customers;
+
+            public CustomerService(IDatabaseSettings settings)
+            {
+                var client = new MongoClient(settings.ConnectionString);
+                var database = client.GetDatabase(settings.DatabaseName);
+
+                _customers = database.GetCollection<Customer>(settings.CollectionName);
+            }
+
+/*            public List<Customer> Get() =>
+                _customers.Find(vendor => true).ToList();
+
+            public Customer Get(string username) =>
+                _customers.Find<Customer>(customer => customer.Username == username).FirstOrDefault();*/
+
+            public Customer Create(Customer customer)
+            {
+                _customers.InsertOne(customer);
+                return customer;
+            }
+
+            public Customer Edit(Customer customer)
+            {
+            var filter = Builders<Customer>.Filter.Eq("_id",customer._id);
+            //var update = Builders<BsonDocument>.Update.Combine(customer);
+            _customers.ReplaceOne(filter, customer);
+            return customer;    
+        }
+      }
+ }
